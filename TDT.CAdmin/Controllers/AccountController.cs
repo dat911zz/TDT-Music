17bf20bb19c13259d3ef1ea8b15c558ab6a38a86
DAT911ZZ\datcy <datcy2011@gmail.com>
2023-07-25 16:57:54 +0700

    Add project files.

---
+++ TDT.CAdmin/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace zadmin_mvc.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
