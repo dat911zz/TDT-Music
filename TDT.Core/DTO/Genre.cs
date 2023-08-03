3aae3d3585a423407f438f0eaa7d801da0fae5cb
thinhcv <thinh.chauvan2405@gmail.com>
2023-08-03 09:18:26 +0700

    update model DTO in core

---
+++ TDT.Core/DTO/Genre.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace TDT.Core.DTO
+{
+    public class Genre // thể loại album // playlist // song
+    {
+        private string _id;
+        private string _name;
+        private string _title;
+        private string _alias;
+        private string _link;
+
+        public string id { get => _id; set => _id = value; }
+        public string name { get => _name; set => _name = value; }
+        public string title { get => _title; set => _title = value; }
+        public string alias { get => _alias; set => _alias = value; }
+        public string link { get => _link; set => _link = value; }
+    }
+}
---
+++ TDT.Core/DTO/TypePlayListDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace TDT.Core.DTO
+{
+    public class TypePlaylistDTO
+    {
+        private string _sectionType;
+        private string _viewType;
+        private string _title;
+        private string _link;
+        private string _sectionId;
+        private Dictionary<string, string> _playlists;
+        private Genre _genre;
+        public TypePlaylistDTO() { }
+
+        public string sectionType { get => _sectionType; set => _sectionType = value; }
+        public string viewType { get => _viewType; set => _viewType = value; }
+        public string title { get => _title; set => _title = value; }
+        public string link { get => _link; set => _link = value; }
+        public string sectionId { get => _sectionId; set => _sectionId = value; }
+        public Dictionary<string, string> playlists { get => _playlists; set => _playlists = value; }
+        public Genre genre { get => _genre; set => _genre = value; }
+    }
+}
