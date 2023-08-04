49b42e08f0366c7e063f42d33f5d93e65079ac7a
thinhcv <thinh.chauvan2405@gmail.com>
2023-08-04 22:08:36 +0700

    update UI site

--- TDT.Site/wwwroot/js/site.js
+++ TDT.Site/wwwroot/js/site.js
@@ -1,4 +1,13 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿function SelectLanguage(item) {
+    if ($(item).hasClass('langen')) {
+        $show = $('.dropdown-item.langvi');
+        $hide = $('.dropdown-item.langen');
+    }
+    else {
+        $hide = $('.dropdown-item.langvi');
+        $show = $('.dropdown-item.langen');
+    }
+    $($show).removeClass('hide');
+    $($hide).addClass('hide');
+    $('#spNameLanguage').html($($hide).children('img').clone());
+}
