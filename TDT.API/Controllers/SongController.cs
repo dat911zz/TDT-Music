ecc1fbd6e7099badedae028660e2a6cddfd0802e
thinhcv <thinh.chauvan2405@gmail.com>
2023-08-04 08:46:37 +0700

    update API

---
+++ TDT.API/Controllers/SongController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using TDT.Core.DTO;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TDT.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongController : ControllerBase
+    {
+        // GET: api/<SongController>
+        [HttpGet]
+        public JsonResult Get()
+        {
+            var item = new SongDTO();
+            item.alias = "hehe-CVT-test";
+            return new JsonResult(item);
+        }
+
+        // GET api/<SongController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<SongController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<SongController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SongController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
