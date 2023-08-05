7ccfcf2858fb23976bf33801e278f0fd4233a4af
thinhcv <thinh.chauvan2405@gmail.com>
2023-08-05 20:00:33 +0700

    update gallery

--- TDT.Site/wwwroot/js/site.js
+++ TDT.Site/wwwroot/js/site.js
@@ -11,3 +11,14 @@
     $($hide).addClass('hide');
     $('#spNameLanguage').html($($hide).children('img').clone());
 }
+
+function ChangeGallery(i) {
+    var gal = $('.gallery-item');
+    $('.gallery-item-first').removeClass('gallery-item-first').addClass('gallery-item-add');
+    $('.gallery-item-previous').removeClass('gallery-item-previous').addClass('gallery-item-first');
+    $('.gallery-item-selected').removeClass('gallery-item-selected').addClass('gallery-item-previous');
+    $('.gallery-item-next').removeClass('gallery-item-next').addClass('gallery-item-selected');
+    $('.gallery-item-last').removeClass('gallery-item-last').addClass('gallery-item-next');
+    $(gal).eq(i).removeClass('gallery-item-add').addClass('gallery-item-last');
+    return i + 1 >= $(gal).length ? 0 : i + 1;
+}
