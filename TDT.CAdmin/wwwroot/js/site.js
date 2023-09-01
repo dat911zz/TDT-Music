c0316fec3cb8c4513bede097d78b08e6c6fc2c80
thinhcv <thinh.chauvan2405@gmail.com>
2023-09-01 19:05:45 +0700

    complete template background color

--- TDT.CAdmin/wwwroot/js/site.js
+++ TDT.CAdmin/wwwroot/js/site.js
@@ -1,18 +1,24 @@
 ﻿$(document).ready(function () {
+    var temp_black = {
+        "--bg-temp": "#12151e", "--bg-main-content": "#12151e", "--nav-link-hover": "#0f1015",
+        "--navbar-shadow": "20px 19px 34px -15px rgba(0, 0, 0, 0.5)", "--color-separate": "#2c2e33",
+        "--color-icon-nav": "#fff", "--color-change": "#fff", "--bg-popup": "#191C24", "--bg-hover": "#0F1015",
+        "--border-temp": "1px px solid #2c2e33", "--popup-shadow": "0px 0px 35px -3px black"
+    };
+    var temp_white = {
+        "--bg-temp": "white", "--bg-main-content": "#F0F2F5", "--nav-link-hover": "#0f1015",
+        "--navbar-shadow": "4px 0 0px 2px rgba(0 0 0 / 0.03)", "--color-separate": "#eaecf0",
+        "--color-icon-nav": "#767676", "--color-change": "#000", "--bg-popup": "#fff", "--bg-hover": "#e5e7eb",
+        "--border-temp": "1px solid #efebeb", "--popup-shadow": "0px 0px 35px -3px #eae4e4"
+    };
     $('.dot-bg-color').on('click', function () {
-        var bg = $(this).css("background-color");
-        $(`body,html,
-            .sidebar-brand-wrapper,
-            .sidebar,
-            .navbar-menu-wrapper,
-            .navbar .navbar-menu-wrapper .search input`).css({ "background-color": bg });
         if ($(this).hasClass('white')) {
-            $('.sidebar-brand-wrapper img').attr("src", '../image/logo_TDT_Devil.png');
-            $('.navbar .navbar-menu-wrapper').css({ "-webkit-box-shadow": "20px 19px 34px -15px rgba(0, 0, 0, 0.05)" });
+            $(":root").css(temp_white);
+            $('.sidebar-brand-wrapper img').attr("src", '../image/logo_TDT_Devil_red.png');
         }
         else {
+            $(":root").css(temp_black);
             $('.sidebar-brand-wrapper img').attr("src", '../image/logo_TDT_Devil-transparent.png');
-            $('.navbar .navbar-menu-wrapper').css({ "-webkit-box-shadow": "20px 19px 34px -15px rgba(0, 0, 0, 0.5)" });
         }
     })
 });
