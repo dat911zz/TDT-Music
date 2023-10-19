a4a76546078b60a2e6e27b1b7987f02d32bdeb4f
thinhcv <thinh.chauvan2405@gmail.com>
2023-10-19 22:07:10 +0700

    update LoadSongRelease

--- TDT.Core/Helper/FirestoreService.cs
+++ TDT.Core/Helper/FirestoreService.cs
@@ -8,6 +8,8 @@ using System.Reactive.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using TDT.Core.DTO.Firestore;
+using TDT.Core.Ultils;
 
 namespace TDT.Core.Helper
 {
@@ -56,8 +58,16 @@ namespace TDT.Core.Helper
         {
             return db.Collection(collectionName).Where(filter);
         }
-        
-        
+        public Query WhereEqualTo(string collectionName, string path, object value)
+        {
+            return db.Collection(collectionName).WhereEqualTo(path, value);
+        }
+        public Query WhereArrayContains(string collectionName, string path, object value)
+        {
+            return db.Collection(collectionName).WhereArrayContains(path, value);
+        }
+
+
         public List<T> Gets<T>(Query query) where T : class, new()
         {
             QuerySnapshot querySnapshot = query.GetSnapshotAsync().Result;
@@ -165,6 +175,14 @@ namespace TDT.Core.Helper
                 Console.WriteLine("-Document {0} does not exist!", snapshot.Id);
                 return null;
             }
+        }
+
+        public string GetIdGenre(string key)
+        {
+            string alias = HelperUtility.GetAlias(key);
+            Query query = WhereEqualTo("Genre", "alias", alias);
+            Genre g = Gets<Genre>(query).First();
+            return g == null ? "" : g.id;
         }
     }
 }
