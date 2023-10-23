7250dab89a1ea20db7eee9bc520e5b871634b682
mimieu-admin <tienyoyoyo@gmail.com>
2023-10-23 15:57:49 +0700

    pull all admin sql, role, perm, group

--- TDT.Core/DTO/GroupDTO.cs
+++ TDT.Core/DTO/GroupDTO.cs
@@ -1,6 +1,7 @@
 ﻿using Swashbuckle.AspNetCore.Annotations;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
@@ -17,8 +18,11 @@ namespace TDT.Core.DTO
         private string _Description;
         private DateTime? _CreateDate;
         [SwaggerSchema(ReadOnly = true)]
+        [Display(Name = "ID")]
         public int Id { get => _Id; set => _Id = value; }
+        [Display(Name = "Tên nhóm người dùng")]
         public string Name { get => _Name; set => _Name = value; }
+        [Display(Name = "Miêu Tả nhóm người dùng")]
         public string Description { get => _Description; set => _Description = value; }
         [SwaggerSchema(ReadOnly = true)]
         public DateTime? CreateDate { get => _CreateDate; set => _CreateDate = value; }
