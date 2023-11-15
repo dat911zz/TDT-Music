642bb2a193258a00f35d0e55bf6a7d899c56d636
mimieu-admin <tienyoyoyo@gmail.com>
2023-11-15 13:16:48 +0700

    comit

--- TDT.Admin.Desktop/Program.cs
+++ TDT.Admin.Desktop/Program.cs
@@ -14,7 +14,6 @@ namespace TDT.Admin.Desktop
         public static QLDV.Controller.UserAuthController controller = null;
         public static QLDV.Views.FrmConfig frmConfig = null;
         public static QLDV.Views.FrmLogin frmLogin = null;
-        public static QLDV.Views.frmLoginNew frmLoginNew = null;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -26,26 +25,21 @@ namespace TDT.Admin.Desktop
             AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(MyExceptionHandler);
             frmConfig = new QLDV.Views.FrmConfig();
             frmLogin = new QLDV.Views.FrmLogin();
-            frmLoginNew = new QLDV.Views.frmLoginNew();
+            frmDashboard = new frmDashboard(frmLogin);
 
-            //controller = QLDV.Controller.UserAuthController.Instance;
-            ////controller.SetConnStr(cfg.GetConnectionString("Admin"));
-            //controller.gotoDashborad = () =>
-            //{
-            //    frmDashboard = new frmDashboard(frmLogin);
-            //    frmDashboard.Show();
-            //    frmLogin.Hide();
-            //};
-            //controller.saveConfig = (serverName, userName, pass, dbName) =>
-            //{
-            //    //ConfigHelper.SaveConfig("Admin", "Data Source=" + serverName + ";Initial Catalog=" + dbName + ";User ID=" + userName + ";pwd = " + pass + "");
-            //};
-            //controller.InitComponents(frmLogin, frmConfig, frmLogin.GetBindings(), frmConfig.GetBindings());
-            //Application.Run(frmLogin);
-
-        
-            Application.Run(frmLoginNew);
-
+            controller = QLDV.Controller.UserAuthController.Instance;
+            //controller.SetConnStr(cfg.GetConnectionString("Admin"));
+            controller.gotoDashborad = () =>
+            {
+                frmDashboard.Show();
+                frmLogin.Hide();
+            };
+            controller.saveConfig = (serverName, userName, pass, dbName) =>
+            {
+                //ConfigHelper.SaveConfig("Admin", "Data Source=" + serverName + ";Initial Catalog=" + dbName + ";User ID=" + userName + ";pwd = " + pass + "");
+            };
+            controller.InitComponents(frmLogin, frmConfig, frmLogin.GetBindings(), frmConfig.GetBindings());
+            Application.Run(frmLogin);
         }
         static void MyExceptionHandler(object sender, UnhandledExceptionEventArgs args)
         {
