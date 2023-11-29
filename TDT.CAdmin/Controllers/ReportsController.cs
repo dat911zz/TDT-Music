7a4d95628ab3d6c1a8b75f1b454570a39bb77cc5
thinhcv <thinh.chauvan2405@gmail.com>
2023-11-29 18:13:07 +0700

    update

---
+++ TDT.CAdmin/Controllers/ReportsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using TDT.CAdmin.CustomReport.PhatHanhBaiHat;
+using TDT.CAdmin.Models;
+using TDT.Core.Models;
+
+namespace TDT.CAdmin.Controllers
+{
+    [Authorize]
+    public class ReportsController : Controller
+    {
+        public IActionResult BaiHatVuaPhatHanh()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult BaiHatVuaPhatHanh(int typeRelease)
+        {
+            string tempFilePath = AppDomain.CurrentDomain.BaseDirectory + "CustomReport/Template/PhatHanhBaiHat.xlsx";
+            PhatHanhBaiHatExcelService service = new PhatHanhBaiHatExcelService((Core.Enums.TypeRelease)typeRelease, tempFilePath);
+            byte[] excel = service.GetExcel();
+            return this.File(excel, "application/vnd.ms-excel", "ReportBaiHatVuaPhatHanh.xlsx");
+        }
+    }
+}
